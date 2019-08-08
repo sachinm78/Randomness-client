b0db7fdc1ba642afaea8bcb2018e86572b56495a
sachinm78 <sachinm78@gmail.com>
2019-08-08 16:34:30 -0500

    edited styling and text for Superheros Container

--- src/containers/SuperHerosContainer.js
+++ src/containers/SuperHerosContainer.js
@@ -43,16 +43,14 @@ class SuperHerosContainer extends Component {
             <div className = 'sh-container'>
                 <img src="https://i2.wp.com/beccabug.com/wp-content/uploads/2012/02/superherologos.jpg?resize=1140%2C682&ssl=1" alt="superhero background"></img>
 
-                <h1>What's Your Random Superhero Name?</h1>
-                <h1> {this.props.state.prefix} {this.props.state.name} {this.props.state.suffix} has the power of {this.props.state.power}</h1>
+                <h1>Generate Your Random Superhero Name and Power!</h1>
+                <h2> {this.props.state.prefix} {this.props.state.name} {this.props.state.suffix}</h2>
+                <h3>has the power of</h3> 
+                <h2>{this.props.state.power}</h2>
                 
-                <h2 >Prefix: {this.props.state.prefix}</h2>
                 <button onClick={e => this.fetchPrefixOnClick(e)}>Change Prefix</button>
-                <h2>Name: </h2>
                 <button onClick={e => this.fetchNameOnClick(e)}>Change Name</button>
-                <h2>Suffix: </h2>
                 <button onClick={e => this.fetchSuffixOnClick(e)}>Change Suffix</button>
-                <h2>Power: </h2>
                 <button onClick={e => this.fetchPowerOnClick(e)}>Change Power</button>
                                 
             </div>
