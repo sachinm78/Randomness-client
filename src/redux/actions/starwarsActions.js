8ef8330cd4b5bf86a801c2e0e5792dedd0e8dd83
sachinm78 <sachinm78@gmail.com>
2019-08-08 18:27:26 -0500

    created starwars actions

---
+++ src/redux/actions/starwarsActions.js
@@ -0,0 +1,37 @@
+
+export const fetchStarwarsData = () => dispatch => {
+    return fetch('http://localhost:3001/starwars')
+    .then(res => res.json())
+    .then(superheros => dispatch({
+        type: 'FETCH_STARWARSDATA_SUCCESS',
+        payload: starwars_data
+    }))
+}
+
+export const fetchRandomCharacter = (character) => {
+    return {
+        type: 'RANDOM_CHARACTER_SUCCESS',
+        payload: character
+    }
+}
+
+export const fetchRandomDroid = (droid) => {
+    return {
+        type: 'RANDOM_DROID_SUCCESS',
+        payload: droid
+    }
+}
+
+export const fetchRandomPlanet = (planet) => {
+    return {
+        type: 'RANDOM_PLANET_SUCCESS',
+        payload: planet
+    }
+}
+
+export const fetchRandomVehicle = (vehicle) => {
+    return {
+        type: 'RANDOM_VEHICLE_SUCCESS',
+        payload: vehicle
+    }
+}
