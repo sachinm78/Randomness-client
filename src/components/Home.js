90d6ff01c20ce5aeeda59091ef88ad954da2c4b4
sachinm78 <sachinm78@gmail.com>
2019-08-09 13:34:48 -0500

    re-styled home page to add text, links and text shadow

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react"
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default () => 
-    
-    <div class='Home-header'>
-        <h1>Let The Randomness Ensue!</h1>
-        
+    <div class='Home-wrapper'>
+        <div class='Home-header'>
+            <h1>Let The Randomness Ensue!</h1>
+        </div>
+        <div class='Home-body'>
+            <h3>Genrate a <Link to = '/superheroes' className = 'Home-link' activeClassName = 'activeLink'>Random Super Hero </Link>Name!</h3>
+            <h3>Take a <Link to = '/starwars' className = 'Home-link' activeClassName = 'activeLink'>Random Star Wars </Link>Adventure!</h3>
+            <h3>More content coming soon...!</h3>
+            
+        </div>
     </div>
