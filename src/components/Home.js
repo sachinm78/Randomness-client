8782011b4add37c376d2baea4bb098a2f11300ba
sachinm78 <sachinm78@gmail.com>
2019-08-10 01:25:00 -0500

    fixed link text on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,9 @@ export default () =>
             <h1>Let The Randomness Ensue!</h1>
         </div>
         <div class='Home-body'>
-            <h3>Genrate a <Link to = '/superheroes' className = 'Home-link' activeClassName = 'Active-link'>Random Super Hero </Link>Name!</h3>
-            <h3>Take a <Link to = '/starwars' className = 'Home-link' activeClassName = 'Active-link'>Random Star Wars </Link>Adventure!</h3>
-            <h3>Generate a <Link to = '/hipster_ipsum' className = 'Home-link' activeClassName = 'Active-link'>Random Hipster Ipsum </Link>Sentence!</h3>
+            <h3>Genrate a <Link to = '/superheroes' className = 'Home-link' activeClassName = 'Active-link'>Random Super Hero</Link> Name!</h3>
+            <h3>Take a <Link to = '/starwars' className = 'Home-link' activeClassName = 'Active-link'>Random Star Wars</Link> Adventure!</h3>
+            <h3>Generate a <Link to = '/hipster_ipsum' className = 'Home-link' activeClassName = 'Active-link'>Random Hipster Ipsum</Link> Sentence!</h3>
             <h3>More content coming soon...!</h3>
         </div>
         <div className="Home-footer">
