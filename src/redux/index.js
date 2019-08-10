a9737e2bbe5b0b045a2735ab3bd7f5b7b60b9cc9
sachinm78 <sachinm78@gmail.com>
2019-08-10 16:19:29 -0500

    imported dev reducer into redux index

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,12 +3,14 @@ import thunk from 'redux-thunk'
 import superheros from './reducers/superherosReducer'
 import starwars_data from './reducers/starwarsReducer'
 import hipster_data from './reducers/hipsterIpsumReducer'
+import dev_data from './reducers/smartDevReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const rootReducer = combineReducers({
     superheros,
     starwars_data,
-    hipster_data
+    hipster_data,
+    dev_data
 })
 
 export default createStore(rootReducer, composeWithDevTools(
